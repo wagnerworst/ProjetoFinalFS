31547c6fa279ed039bfd62e2adfe7665256c6114
Wagner <wagner.worst@gmail.com>
2024-08-28 20:05:39 -0300

    feat: criado as primeiras rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
-import { Button } from 'primereact/button';
+
 
 function App() {
   return (
     <>
       <span className='border-4 text-lg text-white border-white font-bold m-3 rounded-xl p-8 sm flex justify-center items-center'>Começando os trabalhos</span>
-      <Button 
-        label="Primary" 
-        outlined />
+    
     </>
   )
 }
